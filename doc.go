678817dcfe2d43c9bd241e6aee76bd06d6bd8b73
bagaking <zfaustk@qq.com>
2020-11-16 01:25:11 +0800

    doc: demonstrate the different methods of defining typedef-anococo.

--- doc.go
+++ doc.go
@@ -1,9 +1,20 @@
 package anococo
 
 // exampleOfTypeDefAnococo
-// <% memo: this is a sample %>
+// <% memo: this is a sample for typedef parser %>
 // <% btw: if you write a doc site generator, this may be useful %>
 // <% fyi: you can run command below to test it by yourself %>
 // <% e.g. go run ./cmd/anococo t %>
-type exampleOfTypeDefAnococo struct {
+type exampleOfTypeDefAnococo1 struct {
 }
+
+// exampleOfTypeDefAnococo batch
+// <% memo: this is another sample %>
+type (
+	exampleOfTypeDefAnococo2 struct {
+	}
+
+	// <% e.g.: this is sample 3 %>
+	exampleOfTypeDefAnococo3 struct {
+	}
+)
